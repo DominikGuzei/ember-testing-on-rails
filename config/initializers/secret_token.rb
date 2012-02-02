23c9651117855fcd6d9aa42403742cfbde02be79
Dominik Guzei <dominik_guzei@gmx.at>
2012-02-02 21:15:28 +0100

    Creates new rails 3.2 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JsTesting::Application.config.secret_token = 'ec418cab3d1f99cfb2163068f1880a4453462b4b47c0a8cd757b0df11e011c49469a1434d583131e1e8012fdc13be8c76409d2a3792eab3b01c39f674f10f048'
